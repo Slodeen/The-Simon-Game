18164a1a87ca177d469ff999d076381af55b1aea
Sameer Lodeen <SLodeen@Otolaryngology.local>
2020-01-23 16:21:55 -0500

    final codes

--- game.js
+++ game.js
@@ -1,45 +1,95 @@
-
-
-
 var buttonColours = ["red", "blue", "green", "yellow"];
-
 var gamePattern = [];
-
 var userClickedPattern = [];
 
+var started = false;
+var level = 0;
+
+
+$(document).keypress(function(){
+  if (!started){
+    $("#level-title").text("Level" + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+//on click function
+$(".btn").on("click", function(){
+  
+  var userChosenColor = $( this ).attr("id");
+  userClickedPattern.push(userChosenColor);
+  
+  console.log(userChosenColor);
+  
+  playSound(userChosenColor);
+
+  animatePress(userChosenColor);
+
+  checkAnswer(userClickedPattern.length-1);
+ 
+});
+
+//check answer
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length){
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+  }
+}
+
+//creating random and assigning the color
 function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("level" + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-
-  //1. Use jQuery to select the button with the same id as the randomChosenColour
-  //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
+ 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
   playSound(randomChosenColour );
 }
 
 
-$(".btn").on("click", function(){
-
-  var userChosenColor = $( this ).attr("id");
-  
-  userClickedPattern.push(userChosenColor);
-
-  playSound(userChosenColor);
-
-  console.log(userClickedPattern);
-});
-
-
+//Playing sound
 function playSound (name){
- 
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
- 
 };
 
 
+//Animation
+function animatePress(currentColor){
+  $("#" + currentColor).addClass("pressed");
 
+  setTimeout(classRemoved, 100);
+
+    function classRemoved(){
+      $(".btn").removeClass("pressed");
+    }
+  
+  }
+    
+
+
+ function startOver(){
+   level = 0;
+   gamePattern = [];
+   started = false;
+ };
